22fba3ee5411deadc0c3b0e2923639bc47f9ae5f
suparwan <138862546+CasperTao@users.noreply.github.com>
2025-02-07 22:40:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Kamu Yakin?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Yakin?",
+    "Benaran Nih??",
+    "Coba pikirin dulu",
+    "Gausah buru-buru ko",
+    "ko cepet amat mikirnya",
+    "kalo beneran di tolak, parah sih",
+    "Tar gua bakalan sedih",
+    "sedih banget cok",
+    "yaudh lah kalo gitu mah",
+    "Becanda cokk, bilang iya napa ❤️"
 ];
 
 let messageIndex = 0;
